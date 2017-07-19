4e84bd3d392629058bf5888e65f5f39b286e9fa3
nmm199 <marion.mateos@gmail.com>
2017-07-19 16:33:14 +0100

    added updated subgroup functions for kaplan meier survival analysis (e.g km.log.test.sub). functional. 19/7/17 420pm

--- Clinical/clin.script/clinical_data_functions_v2.R
+++ Clinical/clin.script/clinical_data_functions_v2.R
@@ -114,29 +114,30 @@ lin.reg <- function(x,y){
 ## p value for logistic regression
 
 
+#x <- matched.test.pData$age.cat.infant
+#y <- matched.goi.vsd
+#data <- matched.test.pData
+
+
+#logisticRegression <- function(x,y, data) {
+  
+ # temp.glm <- glm (x ~ y, family = binomial (link = 'logit'), data= data)
+ # stats.temp <- cbind(OR=exp(coef(temp.glm)), exp(confint(temp.glm)))
+ # summary.temp <- list (stats.temp, summary(temp.glm)$coefficients, summary(temp.glm)$data$Event)
+ # return(summary.temp)
+#}
+
+
 
 logisticRegression <- function(x,y, data) {
   
-  temp.glm <- glm (x ~ y, family = binomial (link = 'logit'), data= data)
-  return(cbind(OR=exp(coef(temp.glm)), exp(confint(temp.glm)), summary(temp.glm)$coefficients))
+ temp.glm <- glm (x ~ y, family = binomial (link = 'logit'), data= data)
+ return(cbind(OR=exp(coef(temp.glm)), exp(confint(temp.glm)), summary(temp.glm)$coefficients))
 }
 
 
 
-### Note: previously unable to make logistic regression function work, error message "y values must be 0 <= y <= 1", this working function above used glm(x ~y)
-
-# x <- matched.test.pData$age.cat.infant
-# y <- matched.goi.vsd
-# data.source <-  matched.test.pData
-
-# log.reg <- function(x,y,data.source){
-# log.reg.temp <- glm(y ~ x, family = binomial (link = 'logit'), data = data.source)
-# summary.temp <-summary(log.reg.temp)
-# boxplot.temp <- boxplot (y ~ x, col = c("red", "blue"), xlab = "x", ylab = "Biomarker expression", main = "Correlation between biomarker and variable x")
-# }
-
-
-
+### Note: previously unable to make logistic regression function work, error message "y values must be 0 <= y <= 1", this working function above used glm(x ~y) rather than glm(y~x)
 
 
 ###########################################################
@@ -173,11 +174,46 @@ km.log.test <- function(time, event, marker, out.file = "none"){
 }
 
 
+###############################################################
+
+### Function Number 6
+### Function entitled "km.log.test.sub" to create kaplan meier survival curves for age 3-16 year old children treated with curative intent, for chosen MB subgroup(s)
+### input:
+## time (PFS)
+## event (Progression/relapse)
+## marker
+
+### output:
+## km survival curve
+## p values plotted on graph
+## y axis with values as %
+## legend and p value for survival analysis 
+## title to be adjusted according to subgroup, currently for G3/G4 combined
+
+
+km.log.test.sub <- function(time, event, marker, out.file = "none"){
+  if(out.file!="none"){
+    pdf(out.file)
+  }
+  km.PFS.incl <- survfit(Surv(time, event)~marker, type = "kaplan-meier", conf.type = "log")
+  plot(km.PFS.incl, yaxt="n", col = c("red", "blue"),xlab = "time to progression/relapse (years)", ylab = "PFS (%)", xlim = c(0,10), main = "Biomarker expression and progression-free survival (PFS) in G3/G4",  lty = 1:2)
+  PFS.names <- c("biomarker - high", "biomarker - low")
+  legend (x="topright", PFS.names,  lty= 1:2, col = c("red","blue"))
+  axis(2, at=pretty(event), lab=pretty(event) * 100, las=TRUE)
+  PFS.incl.logrank <- survdiff(Surv(time, event) ~ marker)
+  1 - pchisq(PFS.incl.logrank$chisq, length(PFS.incl.logrank$obs)-1) -> surv.p.val
+  text(4,0.1,paste("p =",round(surv.p.val, 3)), pos = 4, cex = 1)
+  if(out.file!="none"){
+    dev.off()
+  }
+}
 
 
 
 
-### Function number 5
+#####################################################################################
+
+### Function number 7
 ### Function entitled "km.log.test.OS"to create kaplan-meier survival curves for OS for children treated age 3-16years with curative intent
 ### input:
 ## time
@@ -209,10 +245,48 @@ km.log.test.OS <- function(time, event, marker, out.file = "none"){
 }
 
 
+#####################################################################################
+
+### Function number 8
+
+### Function named "km.log.test.OS.sub" to examine potential biomarker in subgroup of childhood MB e.g G3G4 combined
+### Function creates kaplan-meier survival curves for OS for children treated age 3-16years with curative intent, within a specified molecular subgroup(s)
+### input:
+## time
+## event
+## marker
+
+### output:
+## km survival curve
+## p values plotted on graph
+## y axis with values as %
+## legend and p value for survival analysis 
+## title to be adjusted for the specific subgroup, currently written for G3G4 combined
+
+
+km.log.test.OS.sub <- function(time, event, marker, out.file = "none"){
+  if(out.file!="none"){
+    pdf(out.file)
+  }
+  km.OS.incl <- survfit(Surv(time, event)~marker, type = "kaplan-meier", conf.type = "log")
+  plot(km.OS.incl,yaxt="n", col = c("red", "blue"),xlab = "overall survival (years)", ylab = "OS (%)", xlim = c(0,10), main = "Biomarker expression and overall survival (OS) in G3/G4",  lty = 1:2)
+  OS.names <- c("biomarker - high", "biomarker - low")
+  legend (x="topright", OS.names,  lty= 1:2, col = c("red","blue"))
+  axis(2, at=pretty(event), lab=pretty(event) * 100, las=TRUE)
+  OS.incl.logrank <- survdiff(Surv(time, event) ~ marker)
+  1 - pchisq(OS.incl.logrank$chisq, length(OS.incl.logrank$obs)-1) -> surv.p.val
+  text(4,0.1,paste("p =",round(surv.p.val, 3)), pos = 4, cex = 1)
+  if(out.file!="none"){
+    dev.off()
+  }
+}
 
 
 
-### Function number 6
+
+#####################################################################################
+
+### Function number 9
 ### Function entitled "cox.result.OS"  to produce cox regression hazard ratio for overall survival in cohort of children
 ### input
 ## time
@@ -239,8 +313,9 @@ cox.result.OS <- function (time, event, marker, strata = NULL)
 }
 
 
+#####################################################################################
 
-### Function Number 7
+### Function Number 10
 
 ### Function entitled "km.log.test.EFS" to create kaplan meier survival curves for EFS for age 3-16 year old children treated with curative intent, MB
 ### Event is defined as death due to any cause, relapse/progression, second malignancy 
@@ -275,8 +350,46 @@ km.log.test.EFS <- function(time, event, marker, out.file = "none"){
 
 
 
+#####################################################################################
 
 
+### Function number 11
 
+### function name "km.log.test.EFS.sub" to test biomarker within subgroup, in this case it will be run in G3/G4 combined
+### same inputs and outputs as parent function "km.log.test.EFS", see below
+
+### Event is defined as death due to any cause, relapse/progression, second malignancy 
+### input:
+## time
+## event
+## marker
+
+### output:
+## km survival curve
+## p values plotted on graph
+## y axis with values as %
+## legend and p value for survival analysis
+## title to be adjusted for the specific subgroup, currently written for G3G4 combined
+
+
+km.log.test.EFS.sub <- function(time, event, marker, out.file = "none"){
+  if(out.file!="none"){
+    pdf(out.file)
+  }
+  km.EFS.incl <- survfit(Surv(time, event)~marker, type = "kaplan-meier", conf.type = "log")
+  plot(km.EFS.incl, yaxt="n", col = c("red", "blue"),xlab = "event-free survival (years)", ylab = "EFS (%)", xlim = c(0,10), main = "Biomarker expression and event-free survival (EFS) in G3/G4",  lty = 1:2)
+  EFS.names <- c("biomarker - high", "biomarker - low")
+  legend (x="topright", EFS.names,  lty= 1:2, col = c("red","blue"))
+  axis(2, at=pretty(event), lab=pretty(event) * 100, las=TRUE)
+  EFS.incl.logrank <- survdiff(Surv(time, event) ~ marker)
+  1 - pchisq(EFS.incl.logrank$chisq, length(EFS.incl.logrank$obs)-1) -> surv.p.val
+  text(4,0.1,paste("p =",round(surv.p.val, 3)), pos = 4, cex = 1)
+  if(out.file!="none"){
+    dev.off()
+  }
+}
+
+
+####################################################################################
 
 
