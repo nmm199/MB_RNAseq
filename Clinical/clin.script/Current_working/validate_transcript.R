f1b4e228af1519ae1061f05addb838ea0ec6cc81
nmm199 <marion.mateos@gmail.com>
2018-10-16 16:42:47 +0100

    working script that now allows validation in G3G4 dataset 16/10/18

--- Clinical/clin.script/Current_working/validate_transcript.R
+++ Clinical/clin.script/Current_working/validate_transcript.R
@@ -153,6 +153,8 @@ eset$q13loss <- eset_match$q13loss_YN
 identical(rownames(pData(eset)),rownames(eset_match)) ### TRUE indicates that rownames are identical therefore valid to use above column additions
 
 View(pData(eset)) 
+summary_cavalli <- summary(pData(matched.eset))
+# write.csv (summary_cavalli, file = "/home/nmm199/R/MB_RNAseq/Clinical/clin.results/April_13_2018/Complete_transcripts_filtered/Validation/summary.cavalli.csv")
 
 # index_match <- match(names(eset_match), rownames(eset))
 # head(index_match)
@@ -227,105 +229,92 @@ length (MELK.low)
  
 ### look at expression in Group 3/Group 4. Generate dataframe for all expression data (rather than just matched.eset for MELK)
 ### rename the matched.eset here
-matched.eset.test <- eset 
+matched.eset.all <- matched.eset 
 
 # sub <- matched.eset.all$Subgroup
+sub <- matched.eset.all@phenoData@data$Subgroup ### an alternate way to access the expression data subcolumns
 
-# matched.eset$LCA  <- ifelse((matched.eset$histology=="LCA"), "LCA", "non-LCA")
-# matched.eset$mets <- ifelse((matched.eset$Met.status_.1.Met._0.M0.=="1"),"metastatic","non-metastatic")
-sub <- matched.eset.test@phenoData@data$Subgroup ### an alternate way to access the expression data subcolumns
-Group3.exp <- matched.eset.test[,which(sub=="Group3")]
-fdata <- featureData(matched.eset.test)
+Group3.exp <- matched.eset.all[,which(sub=="Group3")]
+fdata <- featureData(matched.eset.all)
 Group3.exp@featureData <- fdata ### upload feature data so that it contains data
 pData(Group3.exp) ### 144 samples, all expression features (21641)
 
 
-pData(Group3.Expression)
 ### Group 3 and 4
-G3G4.exp <- matched.eset.test[, which(sub=="Group3"|sub=="Group4")]
-pData(G3G4.exp)
-### creating G3G4 subgroup
+G3G4.exp <- matched.eset.all[, which(sub=="Group3"|sub=="Group4")]
+pData(G3G4.exp)    ### 470 samples, expression set retained
 
-#sub <- matched.eset@phenoData@data$Subgroup
-#sel <- which(sub=="Group3")
-#subset <- sub[sel]
-#sub_expression <- matched.eset[,subset]
-#g3 <- exprs(matched.eset)[,matched.eset@phenoData@data$Subgroup=="Group3"]
-#matched.eset [matched.eset$Subgroup == "Group3", ]
-
-### matched.eset.all is a dataframe that can be used to match any expression set in (goi)
-
-## goi 
+### matched.eset.all is an expression set that can be used to match any expression set in (goi), pData(eset) converts to dataframe
+View(pData(matched.eset.all))
 
 ### create matched G3G4 dataframe with MELK expression data
+### note that MELK is called in from earlier, therefore already in G3G4.exp dataset
 
-matched.eset.G3G4 <- matched.eset.test[index[!is.na(index)],]
-matched.eset.G3G4$MELK   <- eset.exprs["ENSG00000165304", ]
-plot(matched.eset.G3G4$MELK)
+View(pData(G3G4.exp))
 
-plot(matched.eset.G3G4$MELK, xlab = "individual samples", ylab = "MELK expression", main = "Expression of MELK in G3G4 validation cohort")
-abline(h=median(matched.eset.G3G4$MELK), lty = 1, col = "red") ### v for vertical ie x axis ### h, for horizontal
+matched.eset.G3G4 <- pData(G3G4.exp)
+# class(matched.eset.G3G4)
 
-MELK.cat.G3G4 <- ifelse(matched.eset.G3G4$MELK>median(matched.eset.G3G4$MELK, na.rm = T), "high", "low")
-index <- match(names(MELK.cat.G3G4), rownames(G3G4.exp))
-matched.eset.G3G4$MELK.cat <- MELK.cat.G3G4
+
+###############################
+## goi 
+
+### Exploring characteristics of the dataset, note that expression dataset needs the prefix(pData(eset)) to convert to dataframe
+# dim(G3G4.exp)
+# names(pData(G3G4.exp))
+# summary(pData(G3G4.exp))
+# str(pData(G3G4.exp))
+# class(pData(G3G4.exp))
+# rownames(pData(G3G4.exp))
+
+#plot(G3G4.exp$MELKexp)
+#G3G4.exp$MELK
+
+
+plot(matched.eset.G3G4$MELKexp, xlab = "individual samples", ylab = "MELK expression", main = "Expression of MELK in G3G4 validation cohort")
+abline(h=median(matched.eset.G3G4$MELKexp), lty = 1, col = "red") ### v for vertical ie x axis ### h, for horizontal
+
+# MELK.cat.G3G4 <- ifelse(matched.eset.G3G4$MELK>median(matched.eset.G3G4$MELK, na.rm = T), "high", "low")
+# index <- match(names(MELK.cat.G3G4), rownames(G3G4.exp))
+# matched.eset.G3G4$MELK.cat <- MELK.cat.G3G4
 # matched.eset.G3G4$MELKexp <- matched.eset.G3G4$MELK
 
+### create new dataframe to remove the two samples which are coded as SHH_Inf and WNT
+
+matched.eset.G3G4.incl <- matched.eset.G3G4[(matched.eset.G3G4$meth7=="Grp3_LowRisk"|matched.eset.G3G4$meth7=="Grp3_HighRisk"|matched.eset.G3G4$meth7=="Grp4_LowRisk"|matched.eset.G3G4$meth7=="Grp4_HighRisk"),  ]
+# matched.eset.G3G4.excl <- matched.eset.G3G4[(matched.eset.G3G4$meth7=="SHH_Inf"|matched.eset.G3G4$meth7=="WNT"),  ] ### note can also use != nomenclature (does not equal)
+
+                                          
+matched.eset.G3G4.incl$meth7_HR <- matched.eset.G3G4.incl$meth7=="Grp4_HighRisk"|matched.eset.G3G4.incl$meth7 =="Grp3_HighRisk"
 
 # km.OS<- survfit(Surv(time, event)~marker, type = "kaplan-meier", conf.type = "log")
-km.OS.G3G4.MELK <- survfit(Surv(matched.eset.G3G4$OS, matched.eset.G3G4$Dead)~matched.eset.G3G4$MELK.cat, type = "kaplan-meier", conf.type= "log") ### generates same curve as above code
+km.OS.G3G4.MELK <- survfit(Surv(matched.eset.G3G4.incl$OS, matched.eset.G3G4.incl$Dead)~matched.eset.G3G4.incl$MELK, type = "kaplan-meier", conf.type= "log") ### generates same curve as above code
 summary(km.OS.G3G4.MELK)
 plot(km.OS.G3G4.MELK)
 
-### create relevant variables to then use to adjust in multivariable analysis
 
-matched.eset.G3G4$LCA  <- ifelse((matched.eset.G3G4$histology=="LCA"), "LCA", "non-LCA")
-matched.eset.G3G4$mets <- ifelse(matched.eset.G3G4$Met.status_.1.Met._0.M0.=="1","metastatic","non-metastatic")
+### multivariable cox regression analysis for 
+### i) Lancet
+### ii) PNET 5
 
-
-### include in multivariable cox regression analysis
-
-cox.OS.G3G4.MELK <- coxph(Surv(matched.eset.G3G4$OS, matched.eset.G3G4$Dead)~matched.eset.G3G4$MELK + matched.eset.G3G4$LCA + matched.eset.G3G4$Gender + matched.eset.G3G4$S + matched.eset.G3G4$mets, data = matched.eset.G3G4)
+cox.OS.G3G4.MELK.Lancet <- coxph(Surv(matched.eset.G3G4.incl$OS, matched.eset.G3G4.incl$Dead)~matched.eset.G3G4.incl$MELK + matched.eset.G3G4.incl$q13loss + matched.eset.G3G4.incl$meth7_HR +  matched.eset.G3G4.incl$Gender + matched.eset.G3G4.incl$MYC, data = matched.eset.G3G4.incl)
 
 #str(summary(cox.OS.MELK))
 
-cox.n.G3G4 <- summary(cox.OS.G3G4.MELK)[[4]] ### n             ###cox.OS.MELK$n          ### cox.OS.MELK[[11]] 
-cox.nevent.G3G4 <- summary(cox.OS.G3G4.MELK)[[6]] ### nevent   ###cox.OS.MELK$nevent     ### cox.OS.MELK[[12]] 
+cox.n.G3G4.Lancet <- summary(cox.OS.G3G4.MELK.Lancet)[[4]] ### n             ###cox.OS.MELK$n          ### cox.OS.MELK[[11]] 
+cox.nevent.G3G4.Lancet <- summary(cox.OS.G3G4.MELK.Lancet)[[6]] ### nevent   ###cox.OS.MELK$nevent     ### cox.OS.MELK[[12]] 
 
-summary(cox.OS.G3G4.MELK)[[7]] ### this is the table of relevance p value
-cox.pval.G3G4.MELK <- summary(cox.OS.G3G4.MELK)[[7]][1,5] ### this accesses the p value for MELK (row 1, position 5)
-cox.HR.G3G4.MELK <- summary(cox.OS.G3G4.MELK)[[7]][1,2]
-cox.lower.95CI.G3G4.MELK <- summary(cox.OS.G3G4.MELK)[[8]][1,3]
-cox.upper.95CI.G3G4.MELK <- summary (cox.OS.G3G4.MELK)[[8]][1,4]
-summary.cox.G3G4.MELK <- list(pval = cox.pval.MELK, HR = cox.HR.MELK, L95CI = cox.lower.95CI.MELK, U95CI =cox.upper.95CI.MELK, n = cox.n, nevent = cox.nevent, table = summary(cox.OS.MELK)[[7]])  
+summary(cox.OS.G3G4.MELK.Lancet)[[7]] ### this is the table of relevance p value
+cox.pval.G3G4.MELK.Lancet <- summary(cox.OS.G3G4.MELK.Lancet)[[7]][1,5] ### this accesses the p value for MELK (row 1, position 5)
+cox.HR.G3G4.MELK.Lancet <- summary(cox.OS.G3G4.MELK.Lancet)[[7]][1,2]
+cox.lower.95CI.G3G4.MELK.Lancet <- summary(cox.OS.G3G4.MELK.Lancet)[[8]][1,3]
+cox.upper.95CI.G3G4.MELK.Lancet <- summary (cox.OS.G3G4.MELK.Lancet)[[8]][1,4]
+summary.cox.G3G4.MELK.Lancet <- list(pval = cox.pval.G3G4.MELK.Lancet, HR = cox.HR.G3G4.MELK.Lancet, L95CI = cox.lower.95CI.G3G4.MELK.Lancet, U95CI =cox.upper.95CI.G3G4.MELK.Lancet, n = cox.n.G3G4.Lancet, nevent = cox.nevent.G3G4.Lancet, table = summary(cox.OS.G3G4.MELK.Lancet)[[7]], HR_table = summary(cox.OS.G3G4.MELK.Lancet)$conf.int)  
 
+write.csv (summary.cox.G3G4.MELK.Lancet, file = "/home/nmm199/R/MB_RNAseq/Clinical/clin.results/April_13_2018/Complete_transcripts_filtered/Validation/MELK.multivar.G3G4.csv")
 
-### need to get high risk low risk G3G4 status and also to pull in MYC data
-
-eset_df <- as.data.frame(pData(matched.eset))
-Eset_Cavalli <- write.table(eset_df, file = "/home/nmm199/R/MB_RNAseq/Clinical/clin.results/April_13_2018/Complete_transcripts_filtered/Validation/eset.txt")
-
-# str(eset_df)
-MYC <- read.csv(file = "/home/nmm199/R/MB_RNAseq/Clinical/clin.results/April_13_2018/Complete_transcripts_filtered/Validation/Copy_myc_Cav_taylor.csv", header = TRUE)
-
-MYCN <- read.csv (file = "/home/nmm199/R/MB_RNAseq/Clinical/clin.results/April_13_2018/Complete_transcripts_filtered/Validation/Copy_MYCN_Cav_taylor.csv", header = TRUE)
-
-### add in column for amplified to each dataframe
-MYC$Ampl = "1"
-MYCN$Ampl = "1"
-
-
-# head(MYC)
-# head(MYCN)
-# dim(MYCN)
-# dim(MYC)
-
-### match in these Geo accession numbers in to eset_df. 
-### class(MYCN) ### it is a dataframe
-??match
-# eset_ampl_df<- match(eset_df$Sample_geo_accession, MYCN$Accession)
-# match(eset_df$Sample_geo_accession,MYCN$Accession)
-
+# cox.list.95CI <- summary(cox.OS.G3G4.MELK.Lancet)$conf.int
+                                         
 ----------------------------------------------------------------------------------------------
 
 ### example of code that was trialled
@@ -339,5 +328,10 @@ MYCN$Ampl = "1"
 
 # matched.eset <- eset[index[!is.na(index)], ] ### did not work 
 
+### delete below if do not use
 
-
+### example code - work on this if need to alter to bring in additional goi 
+### matched.eset <- eset[index[!is.na(index)],]  ### redefine for which eset group and then may need to alter index 
+# matched.eset.G3G4 <- matched.eset.all [index[!is.na(index)],] ### n=763 (i.e whole cohort) therefore does not work
+# View(pData(matched.eset.G3G4))
+# matched.eset.G3G4$MELK   <- eset.exprs["ENSG00000165304", ]
